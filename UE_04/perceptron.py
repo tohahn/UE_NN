2ef886f228e6ff3488b9f1121a0d12b0f04eb4fe
Tobias Hahn <morowinger@gmail.com>
2016-11-14 15:59:34 +0100

    finished exercise 4 (without additional exercise)

--- UE_04/perceptron.py
+++ UE_04/perceptron.py
@@ -1,34 +1,39 @@
 import numpy as n
 
 def add(w, x, epsilon):
-    return w + ((n.dot((-1*w),x)+epsilon)/n.power(n.linalg.norm(x),2))*x
+    return w + ((n.dot(-1*w, x)+epsilon)/n.power((n.linalg.norm(x)), 2))*x
 
 def sub(w, x, epsilon):
-    print w - ((n.dot((-1*w),x)+epsilon)/n.power(n.linalg.norm(x),2))*x
-    raw_input()
-    return w - ((n.dot((-1*w),x)+epsilon)/n.power(n.linalg.norm(x),2))*x
+    return w - ((n.dot(1*w, x)+epsilon)/n.power((n.linalg.norm(x)), 2))*x
 
-def test(w, x, result, epsilon):
-    if (result == True):
-        if (n.dot(w,x) > 0):
+def test(w, x, epsilon):
+    if (x['res'] > 0):
+        if (n.dot(w,x['data']) > 0):
             return (w,False)
         else:
-            return (add(w, x, epsilon),True)
+            return (add(w, x['data'], epsilon),True)
     else:
-        if (n.dot(w,x) < 0):
+        if (n.dot(w,x['data']) < 0):
             return (w,False)
         else:
-            return (sub(w, x, epsilon),True)
+            return (sub(w, x['data'], epsilon),True)
 
-def learn(data, results, epsilon):
-    w = n.zeros(data.shape[1])
+def learn(data):
+    w = n.zeros(data['data'].shape[1])
     num = data.shape[0]
-    count = 0
+    wrong_count = 0
+    epsilon = 0.001
+    convergence = 0
+    total_count = 0
 
-    for i in range(0,10000):
-        res = test(w, data[n.random.randint(0, num),:], count, epsilon)
+    while (convergence < 1000):
+        random_int = n.random.randint(0, num)
+        res = test(w, data[random_int], epsilon)
         w = res[0]
+        total_count += 1
+        convergence += 1
         if (res[1]):
-            count += 1
+            wrong_count += 1
+            convergence = 0
 
-    return (w,count)
+    return (w,wrong_count,total_count)
--- UE_04/training.py
+++ UE_04/training.py
@@ -1,24 +1,50 @@
 import numpy as n
 import perceptron as p
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.tools.plotting import parallel_coordinates
 
 if __name__ == "__main__":
-    dimensions = 10
     samples = 100
-    epsilon = 0.001
-
-    w = n.random.rand(10) * 20 - 10
-    data = n.random.rand(samples,dimensions) * 20 - 10
-    results = n.zeros(samples)
+    dimensions = 10
+    w = n.random.rand(dimensions) * 20 - 10
+    data = n.zeros(samples, dtype=[('data', 'f8', (dimensions)), ('res', 'f8'), ('names', 'a4')])
+    data['data'] = n.random.rand(samples,dimensions) * 20 - 10
+    print("The original weight vector: {0}".format(w))
 
     for i in range(0,samples):
-        if (n.dot(w, data[i]) > 0):
-            results[i] = 1
+        if (n.dot(w, data['data'][i]) > 0):
+            data['res'][i] = 5
+        elif (n.dot(w, data['data'][i]) < 0):
+            data['res'][i] = -5
         else:
-            results[i] = -1
+            del(data[i])
 
-    res = p.learn(data, results, epsilon)
+    res = p.learn(data)
     w = res[0]
-    print(res)
+    error = 0
+    
+    print("The learned vector: {0}".format(w))
 
     for i in range(0,samples):
-        print("Original result: {0} / Trained Result: {1}".format(results[i], n.dot(w, data[i])))
+        if (data['res'][i] > 0 and n.dot(data['data'][i], w) < 0 or data['res'][i] < 0 and n.dot(data['data'][i], w) > 0):
+            error += 1
+    print("The number of wrong classifications: {0}\nThe number of adjustments made to the vector: {1}\nThe total number of test iterations: {2}".format(error, res[1], res[2]))
+    
+    for i in range(0,samples):
+        if (n.dot(data['data'][i], w) < 0):
+            data['names'][i] = "N/-1"
+        else:
+            data['names'][i] = "P/1"
+    
+    data_columns = ['var1', 'var2', 'var3', 'var4', 'var5', 'var6', 'var7', 'var8', 'var9', 'var10']
+    df = pd.concat([
+        pd.DataFrame(data['data'], columns=data_columns),
+        pd.DataFrame(data['res'], columns=['orig_result']),
+        pd.DataFrame(data['names'], columns=['names'])], axis=1)
+
+    plt.figure()
+
+    parallel_coordinates(df, "names")
+
+    plt.show()
